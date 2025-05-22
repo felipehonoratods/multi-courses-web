8dec04c44049f95d893b9e199987532897d94a2f
felipehonoratods <felipehonoratods@gmail.com>
2025-05-22 12:43:33 -0300

    feat: add App Routes and Layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,19 @@
+import type { PropsWithChildren } from "react";
+
+export const Layout = ({children}: PropsWithChildren) => {
+    return (
+        <div className="bg-gradient-to-br from-background to-muted">
+            header
+            <main className="min-h-screen container mx-auto px-4 py-8">
+                {children}
+            </main>
+            <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
+                <div className="container mx-auto px-4 text-center text-gray-200">
+                    <p>
+                        Made with 💗 by FelipeHonorato.Dev
+                    </p>
+                </div>
+            </footer>
+        </div>
+    );
+};
---
+++ src/router/router.tsx
@@ -0,0 +1,20 @@
+import { Layout } from '@/components/layout';
+import { ThemeProvider } from '@/context/theme-provider';
+import { CityPage } from '@/pages/city-page';
+import { WeatherDashboard } from '@/pages/weather-dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+export function Router() {
+    return (
+        <BrowserRouter>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<WeatherDashboard />} />
+                        <Route path="/city/:cityName" element={<CityPage />} />
+                    </Routes>
+                </Layout>
+            </ThemeProvider>
+        </BrowserRouter>
+    )
+}
