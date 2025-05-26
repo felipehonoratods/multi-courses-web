fb9a3a5c6eaf7bab5c771dddf95d4dc16072c9ca
felipehonoratods <felipehonoratods@gmail.com>
2025-05-26 16:38:07 -0300

    feat: add Setup React Query + Services Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import { Router } from "./router/router";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 function App() {
-  return (<Router/>);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router/>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
